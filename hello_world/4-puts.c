55d40df8a8fff75f4c1fa020bdc5ad564c23acc9
root <sharpsroom@outlook.com>
2024-11-19 15:47:11 +0000

    4=puts.c

--- hello_world/4-puts.c
+++ hello_world/4-puts.c
@@ -1,20 +1,13 @@
 #!/bin/bash
-/*
- * File: 4-puts.c
- * Auth: Brennan D Baraban <375@holbertonschool.com>
- */
-
 #include <stdio.h>
 
 /**
- * main - Prints "Programming is like building a multilingual
- *                puzzle, followed by a new line.
+ * main - Entry point
  *
- * Return: Always 0.
+ * Return: Always 0 (Success)
  */
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
-
 	return (0);
 }
